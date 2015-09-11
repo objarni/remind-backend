75bb52ccdd431996021ee8d1289b1e460880354d
Olof Bjarnason <olof.bjarnason@gmail.com>
2015-09-11 11:52:18 +0200

    removed Vagrant leftovers, simplified structure

---
+++ localserver.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+echo
+echo Staring Remind Backend server. Endpoint: http://localhost:5000
+source venv/bin/activate
+python hello.py
--- src/localserver.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd /home/vagrant/src
-source venv/bin/activate
-# heroku local
-python hello.py
