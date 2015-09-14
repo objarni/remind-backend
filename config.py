29aeb1bfb5c12ed647c068cfffab257ea71ade0c
Olof Bjarnason <olof.bjarnason@gmail.com>
2015-09-14 16:35:49 +0200

    using redis to go config var if it exists, otherwise localhost

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
-redis_url = 'localhost:6379'
+import os
+
+if 'REDISTOGO_URL' in os.environ:
+    redis_url = os.environ['REDISTOGO_URL']
+else:
+    redis_url = 'localhost:6379'
--- localserver.sh
+++ localserver.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 echo
+echo Starting local redis server...
+redis-server &
 echo Staring Remind Backend server. Endpoint: http://localhost:5000
 source venv/bin/activate
 python server.py
