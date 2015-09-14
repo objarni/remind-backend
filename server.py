c310827cedfd28c83de521c73f75706430589bf2
Olof Bjarnason <olof.bjarnason@gmail.com>
2015-09-14 11:44:29 +0200

    ephemeral notes

--- server.py
+++ server.py
@@ -4,19 +4,34 @@ from flask import Flask, jsonify, make_response
 app = Flask(__name__)
 app.debug = True
 
+notes = []
 
-@app.route('/list')
-def hello():
-    notes = [
-        'Brannbara CD nästan slut',
-        'Blogga om bla bla bla',
-        'Glöm inte betala danskursen faktura i mail',
-    ]
-    json = jsonify(msg='hejsan',
-                   notes=notes)
-    response = make_response(json)
+
+def tmpresponse(o):
+    response = make_response(o)
     response.headers['Access-Control-Allow-Origin'] = "*"
     return response
+
+
+@app.route('/add')
+def add_api():
+    notes.append("test count=" + str(len(notes)))
+    return tmpresponse('added')
+
+
+@app.route('/remove_top')
+def remove_top_api():
+    notes.pop()
+    return tmpresponse('remove_top')
+
+
+@app.route('/list')
+def list_api():
+    reversed = list(notes)
+    reversed.reverse()
+    json = jsonify(notes=reversed)
+    return tmpresponse(json)
+
 
 if __name__ == '__main__':
     app.debug = True
