9657a471a923f0d76051f6dd4a4be93a22fed257
Olof Bjarnason <olof.bjarnason@gmail.com>
2015-09-15 14:42:08 +0200

    add/ accepts json data

--- server.py
+++ server.py
@@ -2,7 +2,9 @@
 
 
 # third party
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify
+from flask import request
+from flask.ext.cors import CORS
 import redis
 
 # local
@@ -10,39 +12,44 @@ from config import redis_url
 
 app = Flask(__name__)
 app.debug = True
+CORS(app)
 
 redis = redis.from_url(redis_url)
 
 
-def tmpresponse(o):
-    response = make_response(o)
-    response.headers['Access-Control-Allow-Origin'] = "*"
-    return response
+print "Starting ReMind backend."
 
 
-count = 0
-
-
-@app.route('/add')
+@app.route('/add', methods=["GET", "POST", "OPTIONS"])
 def add_api():
-    global count
-    count += 1
-    value = "test count=%d" % count
-    redis.lpush('notes', value)
-    return tmpresponse('added')
+    print "add called"
+    print "method=" + repr(request.method)
+    if request.method == "POST":
+        print "In POST block"
+        json = request.get_json()
+        print 'json=%s' % json
+        note = json["note"]
+        print 'note=%s' % note
+        redis.lpush('notes', note)
+        return 'added'
+    else:
+        print "In non-POST block"
+        return ''
 
 
 @app.route('/remove_top')
 def remove_top_api():
+    print "remove top called"
     redis.lpop('notes')
-    return tmpresponse('remove_top')
+    return 'remove_top'
 
 
 @app.route('/list')
 def list_api():
+    print "list called"
     result = redis.lrange('notes', 0, 100)
     json = jsonify(notes=result)
-    return tmpresponse(json)
+    return json
 
 
 if __name__ == '__main__':
